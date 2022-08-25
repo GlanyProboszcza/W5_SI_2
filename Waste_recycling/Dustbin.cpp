820268d89fa2332f55f9978ac8b46796f70f05dc
XVND83 <XVND83@XVND83-Z14G8>
2022-08-25 15:31:47 +0200

    dustbin done

--- Waste_recycling/Dustbin.cpp
+++ Waste_recycling/Dustbin.cpp
@@ -1,5 +1,36 @@
 #include "Dustbin.h"
 
+std::exception Dustbin::DustbinContentError() {
+	// ??? nie mam pojecia
+	return ;
+}
+
+void Dustbin::throwOutGarbage(Garbage) {
+	houseWasteContent.push_back(Garbage);
+}
+
+void Dustbin::throwOutPaperGarbage(PaperGarbage) {
+	if (PaperGarbage::isSqueezed == true) {
+		paperContent.push_back(Garbage);
+	}
+	else
+		DustbinContentError();
+}
+
+void Dustbin::throwOutPlasticGarbage(PlasticGarbage) {
+	if (PlasticGarbage::isClean == true) {
+		plasticContent.push_back(Garbage);
+	}
+	else
+		DustbinContentError();
+}
+
+void Dustbin::emptyContents() {
+	paperContent.clear();
+	plasticContent.clear();
+	houseWasteContent.clear();
+}
+
 std::string Dustbin::getColor() {
 	std::string customColor;
 	std::cout << "Enter color of your dustbin: ";
--- Waste_recycling/Dustbin.h
+++ Waste_recycling/Dustbin.h
@@ -1,14 +1,23 @@
 #pragma once
 #include <string>
+#include <vector>
 #include<iostream>
+#include "Garbage.h"
+#include "PaperGarbage.h"
+#include "PlasticGarbage.h"
 
 class Dustbin {
 public:
 	std::string Color;
-	std::string paperContent[10];
-	std::string plasticContent[10];
-	std::string houseWasteContent[10];
-
+	std::vector <std::string> paperContent; // [PaperGarbage] ;
+	std::vector <std::string> plasticContent; // [PlasticGarbage] ;
+	std::vector <std::string> houseWasteContent; // [Garbage] ;
+	
+	std::exception DustbinContentError();
+	void throwOutGarbage(Garbage);
+	void throwOutPaperGarbage(PaperGarbage);
+	void throwOutPlasticGarbage(PlasticGarbage);
+	void emptyContents();
 
 	std::string getColor();
 
